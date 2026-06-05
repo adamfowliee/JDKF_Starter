7936ea1cadd1bad8b01cf45a8e206f3fa8d6a296
adamfowliee <adam.fowlie8@gmail.com>
2026-06-05 16:37:14 +0100

    rough moddel

--- analysing_results.py
+++ analysing_results.py
@@ -1,2 +1,21 @@
 import numpy as np
 
+data = np.load("data/run_model_results.npz")
+
+
+Z = data["Z"]
+predictions = data["predictions"]
+
+mae = np.mean(np.abs(Z[1:] - predictions[:-1]))
+mse = np.mean((Z[1:] - predictions[:-1])**2)
+
+
+print(f'{mae=}')
+print(f'{mse=}')
+
+import matplotlib.pyplot as plt
+
+plt.plot(Z[:,6], label="True Data")
+plt.plot(predictions[:,6], label="Prediction")
+plt.legend()
+plt.show()
